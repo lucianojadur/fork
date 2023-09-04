d8f37ad5109d5dfeb25b76eb068fe5cce12a584d
lucianojadur <ljadur@fi.uba.ar>
2023-09-03 23:42:14 -0300

    implementacion inicial de primes

--- primes.c
+++ primes.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
 int primes(int pipe_l[2]);
 
 
@@ -22,8 +23,9 @@ main(int argc, char *argv[])
 		return 1;
 	}
 
-	int pid = fork();
-	if (pid > 0){
+	pid_t pid = fork();
+
+	if(pid > 0){
 		close(pipe_r[0]);
 		for (int i = 2; i <= max; i++){
 			if (write(pipe_r[1], &i, sizeof(int)) == -1) {
@@ -31,76 +33,87 @@ main(int argc, char *argv[])
 				return 1;
 			}
 		}
-		//close(pipe_r[1]);
-		wait(&pid);
+		close(pipe_r[1]);
+		printf("WR del proceso inicial cerrado\n");
+		wait(0);
 	}
 	else if (pid == 0){
 		close(pipe_r[1]);
 		if (!primes(pipe_r))
 			return 1;
+		close(pipe_r[0]);
 	}
+	else
+		return 1;
 
-	exit(0);
 	return 0;
 }
 
 
 int
 primes(int pipe_l[2])
-{
+{	
+	//printf("inicio lectura y filtro en proceso con PID = %d\n", getpid());
+	close(pipe_l[1]);
 	int p = 0;
 	// leo el primer número del buffer, que es primo
-	if (read(pipe_l[0], &p, sizeof(int)) == -1) {
-		fprintf(stderr, "Error al leer el pipe izquierdo\n");
+	int status = read(pipe_l[0], &p, sizeof(int));
+	if (status <= 0) {
+		if (status == -1)
+			fprintf(stderr, "Error al leer el pipe izquierdo\n");
 		close(pipe_l[0]);
-		close(pipe_l[1]);
+		exit(0);
 		return 0;
 	}
-
 	printf("primo: %d\n", p);
-
+	//
+	// creo el pipe derecho
 	int pipe_r[2];
-	if (pipe_l[0] != EOF){ 
-		if (pipe(pipe_r) == -1) {
-			fprintf(stderr,
-			        "Error al crear el pipe derecho del filtro de %d\n",
-			        p);
-			return 0;
-		}
+	if (pipe(pipe_r) == -1) {
+		fprintf(stderr,
+		        "Error al crear el pipe derecho del filtro de %d\n",
+		        p);
+		return 0;
 	}
-	if (pipe_l[0] == EOF){
-		exit(0);
-	}
-	int pid = fork();
+	
+	pid_t pid = fork();
 	if (pid > 0) {
-		// PARENT process (fork returns child's PID to parent, and 1 to child')
 		close(pipe_r[0]);
-		while (pipe_l[0] != EOF) {
-			int n = 0;
-			if (read(pipe_l[0], &n, sizeof(int)) == -1) {
+		int n = 0;
+		while ((status = read(pipe_l[0], &n, sizeof(int))) > 0){ //pipe_l[0] != EOF) {
+		/*	if (read(pipe_l[0], &n, sizeof(int)) == -1) {
 				fprintf(stderr,
 				        "Error al leer el pipe izquierdo\n");
-				close(pipe_l[0]);
 				return 0;
 			}
-			if (n % p) {  // si n no es divisible por p, viaja
+			// printf("leyendo: %d\n", n);
+		*/	if (n % p) {  // si n no es divisible por p, viaja
 				if (write(pipe_r[1], &n, sizeof(int)) == -1) {
 					fprintf(stderr, "Error al escribir en el pipe derecho del filtro %d", p);
 					return 0;
 				}
+	//			printf("escribo %d en el pipe derecho\n", n);
 			}
 		}
-		//close(pipe_r[1]);	// terminé de leer, cierro el pipe izquierdo
-		wait(&pid);	
+		if (status == -1){
+			fprintf(stderr,
+				    "Error al leer el pipe izquierdo\n");
+			close(pipe_l[0]);
+			close(pipe_r[1]);
+			return 0;
+		}
+	//	printf("llegué al EOF en proceso con PID = %d\n", getpid());
+		
+		close(pipe_l[0]);
+		close(pipe_r[1]);
+		wait(NULL);
 	} else if (pid == 0) {
-		// CHILD process (fork returns 0); creates right filter and pipe pointing to it
-		close(pipe_r[1]);	// cierro la escritura del pipe izquierdo
+		close(pipe_r[1]);
 		return primes(pipe_r);
 	} else {
 		fprintf(stderr, "Error al crear el fork en el filtro de %d\n", p);
 		return 0;
 	}
 	exit(0);
-
 	return 1;
 }
