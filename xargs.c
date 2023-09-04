cda345f254e741eb9d95d78a4bec74644e0bc1f3
luciano jadur <ljadur@fi.uba.ar>
2023-09-04 02:50:33 -0300

    agrego xargs

--- xargs.c
+++ xargs.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
 #ifndef NARGS
 #define NARGS 4
 #endif
@@ -5,7 +11,40 @@
 int
 main(int argc, char *argv[])
 {
-	// Your code here
+	char command[100];
+	char *file;
+	size_t len = 0;
+	char *argv2[NARGS+1];
+	int nread;
 
+	//COMMAND PARSE
+	for (int i = 1; i < argc; i++){
+		strcat(command, argv[i]);
+		strcat(command, " ");
+	}
+	command[strlen(command)-1] = '\0';
+	printf("command to execute: --%s--\n", command);
+
+	//ARGUMENTS/FILES PARSE	
+	argv2[0] = argv[0];
+
+	size_t i = 1;
+	while ((nread = getline(&file, &len, stdin)) != -1 && i < 5) {
+    	char * file_cpy = malloc(nread);
+		strncpy(file_cpy, file, (file[strlen(file)-1] == '\n') ? strlen(file)-1 : strlen(file));
+	//	printf("file copy = --%s--\n", file_cpy);
+		argv2[i] = file_cpy; 
+		i++;
+    }
+	
+	argv2[i] = NULL;
+
+	//for (size_t n = 0; n <= i; n++){
+	//	printf("argv[%lu] = %s\n", n, argv2[n]);
+	//}
+	
+	execvp(argv[1], argv2);
+	
+	printf("error, no se ejecutó exec\n");
 	return 0;
 }
